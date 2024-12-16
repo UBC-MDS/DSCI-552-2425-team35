066ee853b7b4bd3123ad055917d48f664dfa4d98
Long Nguyen <lucius.nguyen@unb.ca>
2024-12-15 21:26:57 -0800

    Add loading messeages in 4 and suppress warning in 3

--- scripts/3_eda.py
+++ scripts/3_eda.py
@@ -9,6 +9,10 @@ import os
 src_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../src"))
 if src_path not in sys.path:
     sys.path.append(src_path)
+import warnings
+from altair.utils.deprecation import AltairDeprecationWarning
+# Suppress 
+warnings.filterwarnings("ignore", category=AltairDeprecationWarning)
 
 import click
 import pandas as pd
